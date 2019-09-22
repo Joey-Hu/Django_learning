117c1210a87b7ad343b2d1789d9e3b3e60c45760
huhao <1925595006@qq.com>
2019-09-22 21:14:38 +0800

    admin界面

--- proj1/book_hero/admin.py
+++ proj1/book_hero/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from book_hero.models import *
 
-# Register your models here.
+
+# 自定义管理界面
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ["id", "btitle", "bpub_date"]
+    list_filter = ["btitle"]
+    search_fields = ["btitle"]
+    list_per_page = 1
+    fieldsets = [
+        ("basic", {"fields": ["btitle"]}),
+        ("more", {"fields": ["bpub_date"]}),
+    ]
+
+
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo)
